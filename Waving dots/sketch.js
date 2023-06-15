c0956327137c78276e2bca9c8b7360cea4432ac0
Joshua Ollivierre <joshino784@gmail.com>
2023-06-15 18:42:48 -0400

    waving dots

--- Waving dots/sketch.js
+++ Waving dots/sketch.js
@@ -17,10 +17,9 @@ function draw()
     {
       for (var y = 0; y < noOfDots; y++)
       {
-        // your code here
         //Calculations for ellipses
-        let circW = x * (size) + 10;
-        let circH = y * (size) + 10;
+        let circX = x * (size/2);
+        let circY = y * (size/2);
 
         // Map the x and y values from noOfDots -1 to 0 and 255 which returns a noise value
         var noiseX = map(x, 0, noOfDots -1, 0, 255);
@@ -28,35 +27,46 @@ function draw()
 
       // Map the result of the noise which will be between 0 and 1 to 0 and 255 for colour
       var r = map(noise(noiseX), 0, 1, 0, 255);
-      var g = map(noise(noiseY), 0, 1, 0, 255);
-      var b = map(noise(noiseX, noiseY), 0, 1, 0, 255);
+      var g = map(noise(noiseY), 0, 1, 0, 10);
+      var b = map(noise(noiseX, noiseY), 0, 1, 0, 50);
 
       //Store the value in col
       var col = color(r,g,b)
-
       // Set the fill color for the ellipse
       fill(red, green, blue);
-      wave(circW, circH, size/2, col); // replace params with the necessary parameters
+      wave(circX, circY, size/2, col, x, y); // replace params with the necessary parameters
         
       }
     }
 }
 
 
-function wave(x, y, size, color) {
+function wave(x, y, size, color, tempx, tempy) {
 
+  // var period = mouseX;
+  // var amplitude = 1 ;
+  // var frequency = 0.5;
+  // var phase = 0;
+  // var angle = frameCount
+   var dot = x+y
+  
   var period = 60;
-  var amplitude = 1;
-  var frequency = 0.1;
-  var phase = 0;
-  var angle = frameCount * 0.1
-  var let = x+y
+    var amplitude = 10;
+    var frequency = 2;
+    var phase = 1;
+    var angle = map(frameCount, 0, 60, 0, 360)
+    var mouseMapX = map(mouseX, 0, width, 0, 50)
+    var mouseMapY = map(mouseY, 0, height, 0, 50)
 
-  var loc = amplitude * sin(angle * let * frequency + phase)
+   var locX = x + cos(angle * 0.5 * frequency + dot +( phase + mouseMapX)) * (amplitude+ mouseMapY);
+   var locY = y + sin(angle * 0.5 * frequency + dot + ( phase + mouseMapX)) * (amplitude + mouseMapY);
 
-  translate(loc,loc)
   fill(color);
-  ellipse(x , y, size);
+  ellipse(x +locX, y + locY , size )
+
+
+
+  
 
 }
 
