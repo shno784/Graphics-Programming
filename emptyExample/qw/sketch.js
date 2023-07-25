f57faa08f3b26b1efaf4f6db813e65a785e59816
Joshua Ollivierre <joshino784@gmail.com>
2023-07-25 14:12:21 -0400

    rt

--- emptyExample/qw/sketch.js
+++ emptyExample/qw/sketch.js
@@ -16,16 +16,21 @@ function setup() {
 function draw() {
   background(0);
 
+  camera(0, -500, -600, 0,0,0,0,1,0)
   push();
   texture(sun);
   rotateY(frameCount)
   sphere(80)
   pop();
+  pointLight(255, 255, 255, 0, 0, 0);
 
+  rotateY(-frameCount/2)
+  translate(250, 0, 0)
   push();
+
   texture(earth)
-  rotateY(frameCount * -2)
-  translate(200, 100, 0)
+ 
+  rotateY(frameCount * 2)
   sphere(50)
   pop();
  
